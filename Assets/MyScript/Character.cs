e06c0d00a2f852168cc447b50316c515698370d6
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-04-27 13:32:31 +0900

    アニメーターの取得、プレイヤーを動かす

---
+++ Assets/MyScript/Character.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    Animator animator;
+
+    [SerializeField] float speed;
+
+    static int hashFront = Animator.StringToHash("Front");
+    static int hashSide = Animator.StringToHash("Side");
+
+    void Awake()
+    {
+        //コンポーネントと関連付け
+        TryGetComponent(out animator);
+    }
+
+    void Start()
+    {
+        //カーソルを中央に固定（マウスを動かすゲーム用）
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        //入力を取得
+        var inputX = Input.GetAxis("Horizontal");
+        var inputY = Input.GetAxis("Vertical");
+        var leftStick = new Vector3(inputX, 0, inputY).normalized;
+
+        //移動速度を計算
+        var velocity = speed * leftStick;
+
+        //Animatorに反映
+        animator.SetFloat(hashFront, velocity.z, 0.1f, Time.deltaTime);
+        animator.SetFloat(hashSide, velocity.x, 0.1f, Time.deltaTime);
+    }
+}
---
+++ Assets/MyScript/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float x;
+    float z;
+    public float moveSpeed = 2f;
+
+    Rigidbody rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        x = Input.GetAxisRaw("Horizontal");
+        z = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector3(x, 0, z) * moveSpeed;
+    }
+}
