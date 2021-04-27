28e674ce39e7cdd5d4cd4a3abbb89140d8208856
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-04-27 23:06:27 +0900

    肩越しカメラの実装

--- Assets/MyScript/Character.cs
+++ Assets/MyScript/Character.cs
@@ -1,12 +1,19 @@
-﻿using System.Collections;
+﻿using Cinemachine;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Character : MonoBehaviour
 {
     Animator animator;
 
     [SerializeField] float speed;
+
+    [Header("Aim")]
+    [SerializeField] Transform eye;
+    [SerializeField] AxisState Vertical;
+    [SerializeField] AxisState Horizontal;
 
     static int hashFront = Animator.StringToHash("Front");
     static int hashSide = Animator.StringToHash("Side");
@@ -29,9 +36,17 @@ public class Character : MonoBehaviour
         var inputX = Input.GetAxis("Horizontal");
         var inputY = Input.GetAxis("Vertical");
         var leftStick = new Vector3(inputX, 0, inputY).normalized;
+        Horizontal.Update(Time.deltaTime);
+        Vertical.Update(Time.deltaTime);
 
         //移動速度を計算
         var velocity = speed * leftStick;
+
+        //向きを更新
+        var horizontalRotation = Quaternion.AngleAxis(Horizontal.Value, Vector3.up);
+        var verticalRotation = Quaternion.AngleAxis(Vertical.Value, Vector3.right);
+        transform.rotation = horizontalRotation;
+        eye.localRotation = verticalRotation;
 
         //Animatorに反映
         animator.SetFloat(hashFront, velocity.z, 0.1f, Time.deltaTime);
