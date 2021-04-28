c984592dd139e79a2866ecdf6ec18c78f6e5b517
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-04-28 19:52:13 +0900

    ドア、オブジェクトを拾う、人と話す

---
+++ Assets/MyScript/Acceptor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Acceptor : MonoBehaviour
+{
+    public abstract void Accept(Visitor visitor);
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
